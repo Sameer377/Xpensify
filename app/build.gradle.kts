6999f3bd1b13d25c1f4b137354d9a024367436ea
Sameer Shaikh <sameer.shaikh.sde@gmail.com>
2024-12-05 17:56:01 +0530

    nav drawer added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,11 +35,13 @@ android {
     }
 }
 
+
 dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
